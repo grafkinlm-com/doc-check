10916e96f59ab7c295c0ca337132c786fab7e483
grafkinlm-com <grafkinlm@gmail.com>
2026-04-13 12:02:24 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,18 @@
+Встроенные модули Python:
+
+os
+re
+zipfile
+subprocess
+logging
+sqlite3
+datetime
+difflib
+Внешние библиотеки:
+
+python-telegram-bot==20.3
+python-docx
+lxml
+Системная утилита:
+
+antiword — для чтения .doc
